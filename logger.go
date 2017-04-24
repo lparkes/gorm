dc83bec57db8f0d9ae2fe023bacec57ebf1dfed0
Lloyd Parkes <lloyd+github@must-have-coffee.gen.nz>
2017-04-25 10:24:13 +1200

    Remove the coloured text
    It is not entirely clear to me what makes other people think that they know what colours look good on my terminal.

--- logger.go
+++ logger.go
@@ -32,15 +32,15 @@ var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 			sql             string
 			formattedValues []string
 			level           = values[0]
-			currentTime     = "\n\033[33m[" + NowFunc().Format("2006-01-02 15:04:05") + "]\033[0m"
-			source          = fmt.Sprintf("\033[35m(%v)\033[0m", values[1])
+			currentTime     = "\n[" + NowFunc().Format("2006-01-02 15:04:05") + "]"
+			source          = fmt.Sprintf("(%v)", values[1])
 		)
 
 		messages = []interface{}{source, currentTime}
 
 		if level == "sql" {
 			// duration
-			messages = append(messages, fmt.Sprintf(" \033[36;1m[%.2fms]\033[0m ", float64(values[2].(time.Duration).Nanoseconds()/1e4)/100.0))
+			messages = append(messages, fmt.Sprintf(" [%.2fms] ", float64(values[2].(time.Duration).Nanoseconds()/1e4)/100.0))
 			// sql
 
 			for _, value := range values[4].([]interface{}) {
@@ -88,9 +88,7 @@ var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 
 			messages = append(messages, sql)
 		} else {
-			messages = append(messages, "\033[31;1m")
 			messages = append(messages, values[2:]...)
-			messages = append(messages, "\033[0m")
 		}
 	}
 
